6c0eaf25dd43f02c012ab99e053b330f5db3f441
AkashDangare911 <akashdangare101@gmail.com>
2026-03-19 00:24:37 +0530

    Created react app with vite and setup tailwind

---
+++ README.md
@@ -0,0 +1,9 @@
+# Steps to create this app
+- npm create vite@latest netflixGPT
+- cd netflixGPT
+- npm install
+- delete un-necessary css, etc. files
+- Setup Tailwind
+    - npm install tailwindcss @tailwindcss/vite --> install
+    - follow steps from here to add tailwind-config in vite.config.js
+- npm run dev
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='flex flex-col justify-center align-middle text-center'>
+      <h1 className="text-3xl">Hello World</h1>
+      <p className="text-lg">This is basic setup for the React app with Tailwind CSS.</p>
+    </div>
+  )
+}
+
+export default App
